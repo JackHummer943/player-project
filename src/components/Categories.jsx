f1cde41bb59d3f21c403016b8cec9a3948c5edce
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2022-12-19 14:55:35 +0300

    player-project0.9

---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+function Categories(){
+  return (          
+  <div class="categories">
+  <ul>
+    <li class="active">Все</li>
+    <li>Мультфильмы</li>
+    <li>Мультсериалы</li>
+    <li>Скетч</li>
+    <li>Фильмы</li>
+    <li>Сериалы</li>
+    <li>Игры</li>
+  </ul>
+</div>
+);
+}
+
+export default Categories;
