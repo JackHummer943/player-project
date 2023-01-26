102c55bc81ebc4dd3d17d11ea806cb85b8addb64
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-01-26 12:02:55 +0300

    23.75

--- src/test.js
+++ src/test.js
@@ -1,13 +1,17 @@
 describe("pow", function() {
 
- function makeTest(x) {
+describe("возводит x в степень 3", function () {
+  
+function makeTest(x) {
   let expected = x * x * x;
   it(`${x} в степени 3 будет ${expected}`, function() {
-    AuthenticatorAssertionResponse.equal(pow(x, 3), expected);
+   assert.equal(pow(x, 3), expected);
   });
  }
  
- for (let x =1; x <= 5; x++) {
+ for (let x = 1; x <= 5; x++) {
   makeTest(x);
  }
+});
+
 });
